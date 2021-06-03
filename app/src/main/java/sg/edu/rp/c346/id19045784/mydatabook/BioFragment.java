af93284d281c738911ee6c9153ca9a71d5fbfd7b
19036308 <19036308@myrp.edu.sg>
2021-06-03 12:26:17 +0800

    Completed BioFragment

--- app/src/main/java/sg/edu/rp/c346/id19045784/mydatabook/BioFragment.java
+++ app/src/main/java/sg/edu/rp/c346/id19045784/mydatabook/BioFragment.java
@@ -1,5 +1,8 @@
 package sg.edu.rp.c346.id19045784.mydatabook;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +10,62 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class BioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BioFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BioFragment newInstance(String param1, String param2) {
-        BioFragment fragment = new BioFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button btnEdit;
+    TextView tvBio;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bio, container, false);
+        View view = inflater.inflate(R.layout.fragment_bio, container, false);
+        btnEdit = view.findViewById(R.id.btnEdit);
+        tvBio = view.findViewById(R.id.tvBioData);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(getActivity());
+
+
+                LinearLayout ll_alert_layout = new LinearLayout(getActivity());
+                ll_alert_layout.setOrientation(LinearLayout.VERTICAL);
+                final EditText ed_input = new EditText(getActivity());
+                ll_alert_layout.addView(ed_input);
+
+                myBuilder.setTitle("Edit Bio");
+                myBuilder.setView(ll_alert_layout);
+                myBuilder.setCancelable(true);
+
+//                EditText myEditText = new EditText(getActivity()); // Pass it an Activity or Context
+//                // Pass two args; must be LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, or an integer pixel value.
+//                myEditText.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT));
+//                myBuilder.setMessage(String.valueOf(myEditText));
+
+                myBuilder.setNeutralButton("Cancel", null);
+//                AlertDialog myDialog = myBuilder.create();
+//                myDialog.show();
+
+                myBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String text = ed_input.getText().toString();
+                        tvBio.setText(text);
+                    }
+                });
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
+        return view;
     }
 }
